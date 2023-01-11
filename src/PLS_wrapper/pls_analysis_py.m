056e90dd041e9eababa450b95a3976c4e69ffb50
neudorf <12356878+neudorf@users.noreply.github.com>
2023-01-11 17:09:11 -0600

    this file is now created by pls_analysis function

--- src/PLS_wrapper/pls_analysis_py.m
+++
@@ -1,3 +0,0 @@
-function result = pls_analysis_py(datamat_lst, num_subj_lst, k, opt)
-    result_tmp = pls_analysis(datamat_lst, num_subj_lst, k, opt);
-    result = rmfield(result_tmp,'field_descrip');
