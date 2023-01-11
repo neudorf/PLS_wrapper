d54f0c3f28c13390fb2c50635b449b1b8371e1fc
neudorf <12356878+neudorf@users.noreply.github.com>
2023-01-11 16:52:56 -0600

    updated pls_analysis_py.m calling strategy

--- src/PLS_wrapper/pls.py
+++ src/PLS_wrapper/pls.py
@@ -1,5 +1,6 @@
 import matlab.engine
 import numpy as np
+import os
 
 class Dict2Object:
     def __init__(self, dict=None):
@@ -126,13 +127,27 @@ def pls_analysis(datamat_lst,num_subj_lst,num_cond,stacked_behavdata,
     ):
     """Python wrapper for matlab implementation of pls_analysis.
     Will use matlab python library to call the original matlab script.
-    Uses the `pls_analysis_py.m` function in the same folder which drops the unsupported character array `field_descrip`.
+    Uses the `pls_analysis_py.m` function in the same folder which drops the 
+    unsupported character array `field_descrip`.
+    Warning that this creates (and removes when finished) a matlab script 
+    called `pls_analysis_py.m` to call `pls_analysis.m` from the matlab PLS 
+    package and removes the `field_descrip` variable (character arrays are 
+    problematic, and this variable is not necessary), so make sure there isn't 
+    a script called `pls_analysis_py.m` in your working directory that you 
+    don't want deleted (not likely but worth mentioning).
+
     Parameters
     ----------
-    datamat_lst         :   list of 2D ndarrays, one for each group. Rows represent independent variable data. If a single 2D ndarray is given will convert to list automatically.
-    num_subj_lst        :   list of integers representing sizes of groups. If a single integer is given will convert to list automatically.
+    datamat_lst         :   list of 2D ndarrays, one for each group. 
+                            Rows represent independent variable data. 
+                            If a single 2D ndarray is given will convert to 
+                            list automatically.
+    num_subj_lst        :   list of integers representing sizes of groups. 
+                            If a single integer is given will convert to list 
+                            automatically.
     num_cond            :   int representing number of conditions.
-    stacked_behavdata   :   2D ndarray with columns representing behavioural data
+    stacked_behavdata   :   2D ndarray with columns representing behavioural 
+                            data
     num_perm            :   int, default=0. Number of permutations.
     num_split           :   int, default=0. Number of split half permutations.
     num_boot            :   int, default=0. Number of bootstrap permutations.
@@ -141,32 +156,51 @@ def pls_analysis(datamat_lst,num_subj_lst,num_cond,stacked_behavdata,
                             within each group. Tells us how condition effects
                             are modulated by group membership. (Boost condition
                             differences, remove overall group diffrences).
-                            1. Remove grand condition means from each group condition
-                            mean. Tells us how conditions are modulated by group 
+                            1. Remove grand condition means from each group 
+                            condition
+                            mean. Tells us how conditions are modulated by 
+                            group 
                             membership (Boost group differences, remove overall
                             condition diffrences).
-                            2. Remove grand mean over all subjects and conditions.
-                            Tells us full spectrum of condition and group effects.
-                            3. Remove all main effects by subtracting condition and
-                            group means. This type of analysis will deal with 
-                            pure group by condition interaction.
+                            2. Remove grand mean over all subjects and 
+                            conditions.
+                            Tells us full spectrum of condition and group 
+                            effects.
+                            3. Remove all main effects by subtracting condition
+                            and group means. This type of analysis will deal
+                            with pure group by condition interaction.
     cormode             :   int, default=0. Correlation type to use.
                             0. Pearson correlation
                             2. covariance
                             4. cosine angle
                             6. dot product
-    boot_type           :   string, default = 'strat'. Use 'nonstrat' for nonstratified boot samples.
-    clim                :   float, default=95.0. Confidence level between 0 and 100.
-    
+    boot_type           :   string, default = 'strat'. Use 'nonstrat' for 
+                            nonstratified boot samples.
+    clim                :   float, default=95.0. Confidence level between 0 and
+                            100.
+
     Return
     ------
-    res_py              :   Object containing same outputs as original matlab script, with same names, converted to python native types.
-                            Matlab single and double arrays are converted to numpy ndarrays.
-                            Floats used as booleans in matlab (0.0 for False, 1.0 for True) are converted to boolean.
-                            Integers or floats being used as integers in matlab converted to int in python.
+    res_py              :   Object containing same outputs as original matlab 
+                            script, with same names, converted to python native
+                            types.
+                            Matlab single and double arrays are converted to 
+                            numpy ndarrays.
+                            Floats used as booleans in matlab (0.0 for False, 
+                            1.0 for True) are converted to boolean.
+                            Integers or floats being used as integers in matlab
+                            converted to int in python.
                             Floats as python floats.
     """
     eng = matlab.engine.start_matlab()
+
+    # Matlab script for calling pls_analysis.m and removing 'field_descrip'
+    temp_script =    'function result = pls_analysis_py(datamat_lst, num_subj_lst, k, opt)\n' \
+                '    result_tmp = pls_analysis(datamat_lst, num_subj_lst, k, opt);\n' \
+                '    result = rmfield(result_tmp,"field_descrip");'
+
+    with open('pls_analysis_py.m','w+') as f:
+        f.write(temp_script)
 
     if type(datamat_lst) is np.ndarray:
         datamat_lst = [datamat_lst.copy()]
@@ -194,4 +228,7 @@ def pls_analysis(datamat_lst,num_subj_lst,num_cond,stacked_behavdata,
 
     res = eng.pls_analysis_py(datamat_lst,num_subj_lst,num_cond,option)
     res_py = PLS_matlab_2_python(res)
+
+    os.remove('pls_analysis_py.m')
+
     return res_py
