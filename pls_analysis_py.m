f84edf488a7c4d7d78b5cc5fbff971512e868762
neudorf <12356878+neudorf@users.noreply.github.com>
2023-01-12 12:25:37 -0600

    can copy this to PLS directory or matlab path

---
+++ pls_analysis_py.m
@@ -0,0 +1,3 @@
+function result = pls_analysis_py(datamat_lst, num_subj_lst, k, opt)
+    result_tmp = pls_analysis(datamat_lst, num_subj_lst, k, opt);
+    result = rmfield(result_tmp,'field_descrip');
